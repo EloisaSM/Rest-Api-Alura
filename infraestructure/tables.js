267665edc78e9839a672f023d9ebee53c9795601
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-01-20 15:37:00 -0300

    add date field on db and moment library

--- infraestructure/tables.js
+++ infraestructure/tables.js
@@ -6,7 +6,7 @@ class Tables {
     }
 
     createAttendance() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL,data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.connection.query(sql, error => {
             if(error) {
--- models/attendance.js
+++ models/attendance.js
@@ -1,10 +1,16 @@
+const moment = require('moment')
+
 const connection = require('../infraestructure/conection')
 
 class Attendance {
     add(attendance) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(attendance.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const dateAttendance = {...attendance,dataCriacao, data}
+
         const sql = 'INSERT INTO Atendimentos SET ?'
 
-        connection.query(sql, attendance, function(error, result) {
+        connection.query(sql, dateAttendance, function(error, result) {
             if(error) {
                 console.log(error)
             }else{
